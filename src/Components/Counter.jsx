81ce82b7847ff97a25782dedbaab906877805a10
Anita <aneeta.ashraf7@gmail.com>
2022-11-01 15:10:27 +0200

    fixed warning

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function Counter(){
     const [count,setCount]= useState(0);
-    useEffect(setTimer, [count]);
+    useEffect(setTimer, [count, handleTick]);
     function setTimer(){
         setTimeout(handleTick, 1000);
     }
